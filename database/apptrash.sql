747ae4d69e3502cc0592d728c8ce63fbb992ca3d
marianatandil <mariana.mezzanotte@gmail.com>
2019-05-20 13:35:26 -0300

    SCRUM0319-22 base de datos DONE

--- database/apptrash.sql
+++ database/apptrash.sql
@@ -7,6 +7,7 @@ CREATE TABLE Reporte (
     id_reporte int NOT NULL AUTO_INCREMENT,
     latitud real  NOT NULL,
     longitud real  NOT NULL,
+    direcciondelHecho varchar(50)  NOT NULL,
     detalle varchar(255)  NULL,
     rutaImagen varchar(255)  NULL,
     rutaVideo varchar(255)  NULL,
@@ -17,12 +18,8 @@ CREATE TABLE Reporte (
     CONSTRAINT reporte_pk PRIMARY KEY (id_reporte)
 );
 
-INSERT INTO `reporte` (`id_reporte`, `latitud`, `longitud`, `detalle`, `rutaImagen`, `rutaVideo`, `fechaCreacion`, `fechaFinalizada`, `patente`, `fk_id_usuario`) VALUES
-(2, -37.3229293823, -59.1214103699, NULL, NULL, NULL, '2019-05-07', NULL, NULL, 1),
-(3, -37.3240550026, -59.1302813474, NULL, NULL, NULL, '2019-05-08', NULL, NULL, 1);
-
 -- Table: usuario
-CREATE TABLE usuario (
+CREATE TABLE Usuario (
     id_usuario int NOT NULL AUTO_INCREMENT,
     dni int  NULL,
     nombre varchar(30)  NOT NULL,
@@ -34,15 +31,14 @@ CREATE TABLE usuario (
     CONSTRAINT usuario_pk PRIMARY KEY (id_usuario)
 );
 
-INSERT INTO `usuario` (`id_usuario`, `dni`, `nombre`, `apellido`, `email`, `contraseña`, `rol`, `direccionUsuario`) VALUES
-(1, NULL, 'juan', 'perez', 'juan@gmail.com', '1234', 1, NULL);
-
 -- foreign keys
 -- Reference: reporte_usuario (table: reporte)
-ALTER TABLE reporte ADD CONSTRAINT reporte_usuario
+ALTER TABLE Reporte ADD CONSTRAINT reporte_usuario
     FOREIGN KEY (fk_id_usuario)
     REFERENCES usuario (id_usuario)
 
 ;
+
+INSERT INTO `usuario` (`id_usuario`, `dni`, `nombre`, `apellido`, `email`, `contraseña`, `rol`, `direccionUsuario`) VALUES (NULL, '24848753', 'Juan', 'Perez', 'juan@gmail.com', '1234', '1', 'Alberdi 222');
 
 -- End of file.
