e4438de1fc806612af8d3cc97f42d7815c5f0db2
mateo-zarrabeitia <mateo.zarrabeitia96@gmail.com>
2019-06-15 10:45:22 -0300

    Cambio Sprint 2, falto pushear.

--- model/WebModel.php
+++ model/WebModel.php
@@ -20,11 +20,22 @@ class WebModel extends Model{
       }
 
       function guardarDenuncia($nombre,$apellido,$dni,$direccionUsuario,$patente,$fecha,$latitud,$longitud,$ruta,$direccionDenuncia) {
-        $usuario = 1;
-        $sentencia = $this->db->prepare('INSERT INTO `reporte` (`latitud`, `longitud`, `direcciondelHecho`, `rutaVideo`, `fechaCreacion`, `patente`, `fk_id_usuario`) VALUES (?,?,?,?,?,?,?)');
-        $resultado =  $sentencia->execute([$latitud,$longitud,$direccionDenuncia,$ruta,$fecha,$patente,$usuario]);
-        return $sentencia->rowCount();
-
-      }
+        $sentencia = $this->db->prepare('SELECT id_usuario FROM usuario WHERE dni = ?');
+      $sentencia->execute([$dni]);
+      $usuario =  $sentencia->fetchAll();
+      if ($usuario) {
+        $id_usuario = $usuario[0]["id_usuario"];
+      } else {
+        $sentencia = $this->db->prepare('INSERT INTO `usuario` (`id_usuario`, `dni`, `nombre`, `apellido`, `email`, `contraseña`, `rol`, `direccionUsuario`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
+        $sentencia->execute([NULL,$dni,$nombre,$apellido,'@gmail.com','1234','0',$direccionUsuario]);
+        $sentencia = $this->db->prepare('SELECT id_usuario FROM usuario WHERE dni = ?');
+        $sentencia->execute([$dni]);
+        $usuario =  $sentencia->fetchAll();
+        $id_usuario = $usuario[0]["id_usuario"];
+      };
+      $sentencia = $this->db->prepare('INSERT INTO `reporte` (`latitud`, `longitud`, `direcciondelHecho`, `rutaVideo`, `fechaCreacion`, `patente`, `fk_id_usuario`) VALUES (?,?,?,?,?,?,?)');
+      $resultado =  $sentencia->execute([$latitud,$longitud,$direccionDenuncia,$ruta,$fecha,$patente,$id_usuario]);
+      return $sentencia->rowCount();
+  }
     }
     ?>
